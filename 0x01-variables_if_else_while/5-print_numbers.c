05cd82728f5a865b022f7ab5d55a1b2ce9e92569
RodneyMwale <rodneymwalr@gmail.com>
2022-12-28 09:26:37 -0800

    0 to 9 using printf

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - Print out numbers 0 to 9 using printf.
+ *Return: Always 0.
+ */
+int main(void)
+{
+	for (int i = 0; i < 10; ++i)
+	printf("%d", i);
+	printf("\n");
+	return (0);
+}
