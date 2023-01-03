0cdd7542172cef56f1a240e2d8020548f052cc25
RodneyMwale <rodneymwalr@gmail.com>
2023-01-03 15:58:23 -0800

    six

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -4,7 +4,7 @@
  * print_line - Draws a straight line according to parameter
  * @n: The number of lines to draw
  * Return: empty
- */
+*/
 void print_line(int n)
 {
 	int x;
@@ -19,5 +19,5 @@ void print_line(int n)
 		{
 			_putchar(95);
 		}
-}
+	}
 }
